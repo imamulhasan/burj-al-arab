f24d281f98aa50ebafb46689297346372538a6b2
unknown <imamulhasanturzo@gmail.com>
2021-10-10 23:09:17 -0700

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyALsJoJo2L5DzRHoimM8MnITTEWoWSX908",
+    authDomain: "burj-al-arab-e5039.firebaseapp.com",
+    projectId: "burj-al-arab-e5039",
+    storageBucket: "burj-al-arab-e5039.appspot.com",
+    messagingSenderId: "660901072940",
+    appId: "1:660901072940:web:ac7951f1be448f1d63c189"
+  };
+
+  export default firebaseConfig;
